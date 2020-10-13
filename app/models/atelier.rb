96220458220b0d78cfb5c89ea69c8576171f3a0d
Yann Lauwers <yann.lauwers@icloud.com>
2020-10-13 19:15:44 +0200

    je dois maintenant régler la hauteur de chaque page afin qu'elle s'adapte au contenu..

--- app/models/atelier.rb
+++ app/models/atelier.rb
@@ -2,7 +2,7 @@ class Atelier < ApplicationRecord
 
   belongs_to :user
 
-  validates :title, presence :true
-  validates :price, presence :true
+  validates :title, presence: true
+  validates :price, presence: true
 
 end
