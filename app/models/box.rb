475e9e2b81a19619ac4431605a8aa817e67ba8ef
Yann Lauwers <yann.lauwers@icloud.com>
2020-10-21 15:54:59 +0200

    changed writting of box model

--- app/models/box.rb
+++ app/models/box.rb
@@ -2,7 +2,7 @@ class Box < ApplicationRecord
 
   belongs_to :user
 
-  validates :title, presence :true
-  validates :price, presence :true
+  validates :title, presence: true
+  validates :price, presence: true
 
 end
