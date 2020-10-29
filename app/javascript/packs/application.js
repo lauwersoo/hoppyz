a804f0b58cdf4ada8d92b43d7139eeacf4efba83
Yann Lauwers <yann.lauwers@icloud.com>
2020-10-29 10:59:53 +0100

    rials ujs now works perfectly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "bootstrap";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -15,6 +12,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Write all the functions to import in that file so rails ujs isnt loaded many times.
+require("packs/custom")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,15 +21,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import { backToTop } from "./back_to_top"
-import { beerScrolling } from "./beer_scrolling"
-
-if (document.getElementById("back-to-top-anchor")) {
-    backToTop();
-}
-if (document.querySelector(".cup")) {
-    beerScrolling();
-}
-
-// javascript on scroll animations
-AOS.init();
